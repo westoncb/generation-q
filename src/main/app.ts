15c53604a344befe76a200d88da3f90f3d9c9c2d
Weston C. Beecroft <westoncb@gmail.com>
2022-09-06 22:21:08 -0700

    autoformat ran on a couple files

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,9 +1,9 @@
-import { app, BrowserWindow } from 'electron';
-import { createAppWindow } from './appWindow';
+import { app, BrowserWindow } from "electron"
+import { createAppWindow } from "./appWindow"
 
 /** Handle creating/removing shortcuts on Windows when installing/uninstalling. */
-if (require('electron-squirrel-startup')) {
-  app.quit();
+if (require("electron-squirrel-startup")) {
+    app.quit()
 }
 
 /**
@@ -11,7 +11,7 @@ if (require('electron-squirrel-startup')) {
  * initialization and is ready to create browser windows.
  * Some APIs can only be used after this event occurs.
  */
-app.on('ready', createAppWindow);
+app.on("ready", createAppWindow)
 
 /**
  * Emitted when the application is activated. Various actions can
@@ -19,28 +19,28 @@ app.on('ready', createAppWindow);
  * attempting to re-launch the application when it's already running,
  * or clicking on the application's dock or taskbar icon.
  */
-app.on('activate', () => {
-  /**
-   * On OS X it's common to re-create a window in the app when the
-   * dock icon is clicked and there are no other windows open.
-   */
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createAppWindow();
-  }
-});
+app.on("activate", () => {
+    /**
+     * On OS X it's common to re-create a window in the app when the
+     * dock icon is clicked and there are no other windows open.
+     */
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createAppWindow()
+    }
+})
 
 /**
  * Emitted when all windows have been closed.
  */
-app.on('window-all-closed', () => {
-  /**
-   * On OS X it is common for applications and their menu bar
-   * to stay active until the user quits explicitly with Cmd + Q
-   */
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+app.on("window-all-closed", () => {
+    /**
+     * On OS X it is common for applications and their menu bar
+     * to stay active until the user quits explicitly with Cmd + Q
+     */
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
 
 /**
  * In this file you can include the rest of your app's specific main process code.
